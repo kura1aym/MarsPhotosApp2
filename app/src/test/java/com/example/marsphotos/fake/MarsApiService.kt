f0c8bbdadf5bda4fc56b1afe9ceeeb8130b2a9e1
kura1aym <mukhtarkuralay249@mail.ru>
2023-12-02 04:33:56 +0600

    final commit

--- app/src/test/java/com/example/marsphotos/fake/MarsApiService.kt
+++
@@ -1,7 +0,0 @@
-package com.example.marsphotos.fake
-
-class FakeMarsApiService : MarsApiService {
-    override suspend fun getPhotos(): List<MarsPhoto> {
-        return FakeDataSource.photosList
-    }
-}

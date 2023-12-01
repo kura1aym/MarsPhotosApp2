37b20c1259ed4050d11224f8b31fae5ed5403ba7
kura1aym <mukhtarkuralay249@mail.ru>
2023-12-02 00:08:02 +0600

    Create an Application Container

--- app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -16,6 +16,9 @@
 
 package com.example.marsphotos.network
 
+import com.example.marsphotos.data.AppContainer
+import com.example.marsphotos.data.MarsPhotosRepository
+import com.example.marsphotos.data.NetworkMarsPhotosRepository
 import com.example.marsphotos.model.MarsPhoto
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
@@ -23,30 +26,32 @@ import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 import retrofit2.http.GET
 
-    private const val BASE_URL =
+
+class DefaultAppContainer : AppContainer {
+
+    private val baseUrl =
         "https://android-kotlin-fun-mars-server.appspot.com"
 
-    /**
-     * Use the Retrofit builder to build a retrofit object using a kotlinx.serialization converter
-     */
     private val retrofit = Retrofit.Builder()
         .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
-        .baseUrl(BASE_URL)
+        .baseUrl(baseUrl)
         .build()
 
-    /**
-     * Retrofit service object for creating api calls
-     */
+    private val retrofitService: MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java)
+    }
+
+    override val marsPhotosRepository: MarsPhotosRepository by lazy {
+        NetworkMarsPhotosRepository(retrofitService)
+    }
+}
+
+
     interface MarsApiService {
         @GET("photos")
         suspend fun getPhotos(): List<MarsPhoto>
     }
 
-    /**
-     * A public Api object that exposes the lazy-initialized Retrofit service
-     */
-    object MarsApi {
-        val retrofitService: MarsApiService by lazy {
-            retrofit.create(MarsApiService::class.java)
-        }
-}
+
+
+
